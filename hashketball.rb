1803a4cd4bfadaba4e418167e9b12368645bffcb
Samuel Lesser <ineedmoreplates@gmail.com>
2020-05-08 20:10:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,12 +190,10 @@ def team_names
     end
     
 def player_numbers(team)
-var =
 
 game_hash.each do |key, value|
  if team == game_hash[key][:team_name]
-    var = gather_numbers(key)
+    puts gather_numbers(key)
     end  
   end    
-var
 end
