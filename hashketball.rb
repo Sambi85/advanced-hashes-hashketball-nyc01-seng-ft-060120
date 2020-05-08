e6f35fec03ea30dcfa64fec3c8be586bbf972340
Samuel Lesser <ineedmoreplates@gmail.com>
2020-05-08 20:06:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -144,18 +144,18 @@ array << game_hash[:away][:team_name]
 array
 end
 
-def gather_numbers
+def gather_numbers(var)
 array= []
   i = 0
-    while i < game_hash[:home][:players].length
-      array <<  game_hash[:home][:players][i][:number]
+    while i < game_hash[var][:players].length
+      array <<  game_hash[var][:players][i][:number]
       
       i += 1
     end
   array
 end
 
-pp gather_numbers
+pp gather_numbers(:home)
 
 # ------------------------------------------------#
 
