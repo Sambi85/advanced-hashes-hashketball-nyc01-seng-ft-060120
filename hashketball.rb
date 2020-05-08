39826020991277d88f14a1106f279202859bca9c
Samuel Lesser <ineedmoreplates@gmail.com>
2020-05-08 19:57:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def team_names
     end
     
 def player_numbers(team)
-pp  :team_name
+pp  [:home]:team_name
 game_hash.each do |key, value|
  if team == game_hash[key][:team_name]
     gather_numbers
